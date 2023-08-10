8adb25573757a521eb65ec2710f29f8dbc6f3fcd
Trayana Hansen <92487819+trayana-hansen@users.noreply.github.com>
2023-08-10 08:26:11 +0200

    nav bar

--- test/src/compenents/responsive-navbar-1.jsx
+++ test/src/compenents/responsive-navbar-1.jsx
@@ -1,30 +1,44 @@
-import './responsive-navbar-1.scss'
-import React from "react";
+import "./responsive-navbar-1.scss";
+import React, { useState } from "react";
 
 const Navbar1 = () => {
-    return (
-        <div>
-            <div className="container">
-  <div className="row">
-    <div className="menu-btn">
-      <i className="fa-solid fa-bars"></i>
-    </div>
-    <nav className="navbar">
-      <div className="logo">
+  const [isNavExpanded, setIsNavExpanded] = useState(false);
+  return (
+    <div>
+      <div className="container">
+        <div
+          className="menu-btn"
+          onClick={() => {
+            setIsNavExpanded(!isNavExpanded);
+          }}
+        >
+          <i className="fa-solid fa-bars"></i>
+          <nav className="navbar">
+            <div className="logo">
+              <a href="">Your Logo</a>
+            </div>
 
-        <a href="">Your Logo</a>
-      </div>
-      <ul className="main-menu">
-        <li className="list-items"><a href="">About</a></li>
-        <li className="list-items"><a href="">Services</a></li>
-        <li className="list-items"><a href="">Contact</a></li>
-        <li className="list-items"><a href="">Blog</a></li>
-      </ul>
-    </nav>
-  </div>
-
-</div>
+            <ul
+              className={`main-menu
+          isNavExpanded ? "navigation-menu expanded" : "navigation-menu"`}
+            >
+              <li className="list-items">
+                <a href="">About</a>
+              </li>
+              <li className="list-items">
+                <a href="">Services</a>
+              </li>
+              <li className="list-items">
+                <a href="">Contact</a>
+              </li>
+              <li className="list-items">
+                <a href="">Blog</a>
+              </li>
+            </ul>
+          </nav>
         </div>
-      )
-    }
-    export default Navbar1
+      </div>
+    </div>
+  );
+};
+export default Navbar1;
