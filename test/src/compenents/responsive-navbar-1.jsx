679b6c21d08c3c3b115005492dc2073503fb0b46
Trayana Hansen <92487819+trayana-hansen@users.noreply.github.com>
2023-08-10 13:21:44 +0200

    changes

--- test/src/compenents/responsive-navbar-1.jsx
+++ test/src/compenents/responsive-navbar-1.jsx
@@ -2,41 +2,11 @@ import "./responsive-navbar-1.scss";
 import React, { useState } from "react";
 
 const Navbar1 = () => {
-  const [isNavExpanded, setIsNavExpanded] = useState(false);
   return (
-    <div>
+    <div className="App">
+      <Navbar1 />
       <div className="container">
-        <div
-          className="menu-btn"
-          onClick={() => {
-            setIsNavExpanded(!isNavExpanded);
-          }}
-        >
-          <i className="fa-solid fa-bars"></i>
-          <nav className="navbar">
-            <div className="logo">
-              <a href="">Your Logo</a>
-            </div>
-
-            <ul
-              className={`main-menu
-          isNavExpanded ? "navigation-menu expanded" : "navigation-menu"`}
-            >
-              <li className="list-items">
-                <a href="">About</a>
-              </li>
-              <li className="list-items">
-                <a href="">Services</a>
-              </li>
-              <li className="list-items">
-                <a href="">Contact</a>
-              </li>
-              <li className="list-items">
-                <a href="">Blog</a>
-              </li>
-            </ul>
-          </nav>
-        </div>
+        <h1>Burger menu</h1>
       </div>
     </div>
   );
