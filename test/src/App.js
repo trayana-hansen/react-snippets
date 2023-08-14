523d0ada49f4324013828e231b7d67c901b2e9ea
Trayana Hansen <92487819+trayana-hansen@users.noreply.github.com>
2023-08-14 08:26:51 +0200

    nav bar responsive

--- test/src/App.js
+++ test/src/App.js
@@ -1,12 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
-import Navbar1 from './compenents/responsive-navbar-1';
+import "./App.css";
+import Navbar1 from "./compenents/responsive-navbar-1";
 
 function App() {
   return (
-    <div className="App">
-      <h1>fa</h1>
+    <div>
       <Navbar1 />
+      <div className="container">
+        <h1>Something</h1>
+      </div>
     </div>
   );
 }
--- test/src/compenents/responsive-navbar-1.jsx
+++ test/src/compenents/responsive-navbar-1.jsx
@@ -2,13 +2,39 @@ import "./responsive-navbar-1.scss";
 import React, { useState } from "react";
 
 const Navbar1 = () => {
+  const [isNavExpanded, setIsNavExpanded] = useState(false);
   return (
-    <div className="App">
-      <Navbar1 />
-      <div className="container">
-        <h1>Burger menu</h1>
+    <nav className="navigation">
+      <a href="/" className="brand-name">
+        Brand Name
+      </a>
+      <button
+        className="hamburger"
+        onClick={() => {
+          setIsNavExpanded(!isNavExpanded);
+        }}
+      >
+       
+        <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M4 18L20 18" stroke="#000000" stroke-width="2" stroke-linecap="round"></path> <path d="M4 12L20 12" stroke="#000000" stroke-width="2" stroke-linecap="round"></path> <path d="M4 6L20 6" stroke="#000000" stroke-width="2" stroke-linecap="round"></path> </g></svg>
+      </button>
+      <div
+        className={
+          isNavExpanded ? "navigation-menu expanded" : "navigation-menu"
+        }
+      >
+        <ul>
+          <li>
+            <a href="/home">Home</a>
+          </li>
+          <li>
+            <a href="/about">About</a>
+          </li>
+          <li>
+            <a href="/contact">Contact</a>
+          </li>
+        </ul>
       </div>
-    </div>
+    </nav>
   );
 };
 export default Navbar1;
